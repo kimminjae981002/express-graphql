946f60d2ab82955231019646117755e802ab7264
minjae Kim <cmg981548@gmail.com>
2024-04-08 22:41:41 +0900

    feat: [main] mutation을 이용한 댓글 생성

--- comments/comments.resolvers.js
+++ comments/comments.resolvers.js
@@ -11,4 +11,9 @@ module.exports = {
     // parent가 필요없으면 _를 넣어주자
     // passport done의 null 값과 같다
   },
+  Mutation: {
+    addNewComment: (_, args) => {
+      return commentsModel.addNewComment(args.id, args.text, args.likes);
+    },
+  },
 };
