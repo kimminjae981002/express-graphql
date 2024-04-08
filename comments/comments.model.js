cd58d367d47156bad1b78f39470ed3559b74fe43
minjae Kim <cmg981548@gmail.com>
2024-04-08 22:06:52 +0900

    feat: [main] 좋아요 필터링 기능 추가

--- comments/comments.model.js
+++ comments/comments.model.js
@@ -4,12 +4,24 @@ const comments = [
     text: "댓글 부분",
     likes: 1,
   },
+  {
+    id: "comment 2",
+    text: "댓글 부분 2",
+    likes: 10,
+  },
 ];
 
 function getAllComments() {
   return comments;
 }
 
+function getCommentsByLikes(minLikes) {
+  return comments.filter((comment) => {
+    return comment.likes >= minLikes;
+  });
+}
+
 module.exports = {
   getAllComments,
+  getCommentsByLikes,
 };
--- comments/comments.resolvers.js
+++ comments/comments.resolvers.js
@@ -5,5 +5,10 @@ module.exports = {
     comments: () => {
       return commentsModel.getAllComments();
     },
+    commentsByLikes: (_, args) => {
+      return commentsModel.getCommentsByLikes(args.minLikes);
+    },
+    // parent가 필요없으면 _를 넣어주자
+    // passport done의 null 값과 같다
   },
 };
