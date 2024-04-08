8cadadbc892cdf2b8ba0dc2e5fef73fd277b1fdf
minjae Kim <cmg981548@gmail.com>
2024-04-08 21:31:12 +0900

    feat: [main] resolver 함수 생성

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ const loadedFiles = loadFilesSync("**/*", {
 
 const schema = makeExecutableSchema({
   typeDefs: loadedFiles,
+  resolvers: {
+    Query: {
+      posts: async (parent, args, context, info) => {
+        // console.log("parent", parent); // 이 필드의 부모에 대한 resolver의 반환 값
+        // console.log("args", args); // 이 필드에 제공된 모든 GraphQL 인수를 포함하는 객체 // postsId 1 필터링을 해준다.
+        // console.log("context", context); // 특정 작업에 대해 실행 중인 모든 resolver 간에 공유되는 object
+        // console.log("info", info); //
+        const posts = await Promise.resolve(parent.posts);
+        return posts; // parent에 있는 posts를 보여준다
+      },
+      comments: async (parent) => {
+        const comments = await Promise.resolve(parent.comments);
+        return await comments; // parent에 있는 comments를 보여준다
+      },
+    },
+  },
 });
 
 const root = {
