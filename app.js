060c99d6d0c2721ea1c5533d2c3b50fd915d795c
minjae Kim <cmg981548@gmail.com>
2024-04-08 20:49:59 +0900

    feat: [main] posts comments 스키마 이용하기

--- app.js
+++ app.js
@@ -6,13 +6,59 @@ const app = express();
 const PORT = 3000;
 
 const schema = buildSchema(`
-    type Query{
-      description: String
-}`);
+    type Query {
+      posts: [Post],
+      comments: [Comment]
+     }
+
+     type Post {
+        id: ID!
+        title: String!
+        description: String!
+        comments: [Comment]
+     }
+
+     type Comment {
+        id: ID!
+        text: String!
+        likes: Int
+     }
+
+
+
+`);
+// !를 붙이면 있어야된다.
 // description을 요청하면 root값이 나온다.
 
 const root = {
-  description: "hello world!",
+  posts: [
+    {
+      id: "post1",
+      title: "It is a first post",
+      description: "설명 부분",
+      comments: [
+        {
+          id: "comment 1",
+          text: "댓글 부분",
+          likes: 1,
+        },
+      ],
+    },
+    {
+      id: "post2",
+      title: "It is a second post",
+      description: "설명 부분",
+      comments: [],
+    },
+  ],
+
+  comments: [
+    {
+      id: "comment 1",
+      text: "댓글 부분",
+      likes: 1,
+    },
+  ],
 };
 // description을 요청하면 root값이 나온다.
 
