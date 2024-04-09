568ef3c9c2675136a151012263a8c76e56e66bb5
minjae Kim <cmg981548@gmail.com>
2024-04-09 13:52:10 +0900

    feat: [main] Apollo Server 로직 구현

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const { loadFiles, loadFilesSync } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const express = require("express");
 // const { graphqlHTTP } = require("express-graphql");
 // const { buildSchema } = require("graphql");
 
-const app = express();
 const PORT = 3000;
-
-// !를 붙이면 있어야된다.
-// description을 요청하면 root값이 나온다.
 
 const loadedFiles = loadFilesSync("**/*", {
   extensions: ["graphql"],
@@ -21,10 +18,35 @@ const loadedResolverFiles = loadFilesSync(
 );
 // 절대경로로 resolvers.js 파일 불러오기
 
-const schema = makeExecutableSchema({
-  typeDefs: loadedFiles,
-  resolvers: loadedResolverFiles,
-});
+async function startApolloServer() {
+  // express도 Apollo에 접근해야해서
+  const app = express();
+
+  const schema = makeExecutableSchema({
+    typeDefs: loadedFiles,
+    resolvers: loadedResolverFiles,
+  });
+
+  // apollo 객체 생성
+  const server = new ApolloServer({
+    schema,
+  });
+
+  // apollo server 시작
+  await server.start();
+
+  // express와 연결
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(PORT, (req, res) => {
+    console.log(`server open ${PORT}`);
+  });
+}
+
+startApolloServer();
+
+// !를 붙이면 있어야된다.
+// description을 요청하면 root값이 나온다.
 
 // const root = {
 //   posts: require("./posts/posts.model"),
@@ -34,7 +56,3 @@ const schema = makeExecutableSchema({
 
 // app.use("/graphql", graphqlHTTP({ schema })); // express-graphql
 // post /graphql을 요청하면 descrition에서 root가 나온다.
-
-app.listen(PORT, (req, res) => {
-  console.log(`server open ${PORT}`);
-});
